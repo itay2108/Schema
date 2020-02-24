399876b8d54e6d00177668c142d54296f1cbafdc
itay <gervash@icloud.com>
2020-02-24 03:12:38 +0200

    Fix schema order causing wrong results

--- Schema/Models/Info.swift
+++ Schema/Models/Info.swift
@@ -11,62 +11,64 @@ import Foundation
 class Info {
     
     let titles = [
+        "Emotional Deprivation",
         "Abandonment / Instability",
         "Mitrust / Abuse",
-        "Emotional / Deprivation",
-        "Defectiveness / Shame",
         "Social Isolation",
+        "Defectiveness / Shame",
+        "Failure to achieve",
         "Dependence / Incompetence",
         "Vulnerability",
         "Undeveloped self",
-        "Failure to achieve",
-        "Entitlement / Grandiosity",
-        "Insufficient self-control",
         "Subjugation",
         "Self Sacrifice",
-        "Approval seeking",
-        "Negativity",
         "Emotional Inhibition",
         "Hypercriticalness",
+        "Entitlement / Grandiosity",
+        "Insufficient self-control",
+        "Approval seeking",
+        "Negativity",
         "Punitiveness"
     ]
     
     let schemeDescriptions = [
         
+        "Expectation that one's desire for a normal degree of emotional support will not be adequately met by others.  The three major forms of deprivation are: \nA. Deprivation of Nurturance:  Absence of attention, affection, warmth, or companionship. \nB. Deprivation of Empathy:  Absence of understanding, listening, self-disclosure, or mutual sharing of feelings from   others. \nC. Deprivation of Protection:  Absence of strength, direction, or guidance from others.",
+        
         "The perceived instability or unreliability of those available for support and connection. Involves the sense that significant others will not be able to continue providing emotional support, connection, strength, or practical protection because they are emotionally unstable and unpredictable (e.g., angry outbursts), unreliable, or erratically present; because they will die imminently; or because they will abandon the patient in favor of someone better.",
         
         "The expectation that others will hurt, abuse, humiliate, cheat, lie, manipulate, or take advantage.  Usually involves the perception that the harm is intentional or the result of unjustified and extreme negligence. May include the sense that one always ends up being cheated relative to others or 'getting the short end of the stick.'",
         
-        "Expectation that one's desire for a normal degree of emotional support will not be adequately met by others.  The three major forms of deprivation are: \nA. Deprivation of Nurturance:  Absence of attention, affection, warmth, or companionship. \nB. Deprivation of Empathy:  Absence of understanding, listening, self-disclosure, or mutual sharing of feelings from   others. \nC. Deprivation of Protection:  Absence of strength, direction, or guidance from others.",
+        "The feeling that one is isolated from the rest of the world, different from other people, and/or not part of any group or community.",
         
         "The feeling that one is defective, bad, unwanted, inferior, or invalid in important respects; or that one would be unlovable to significant others if exposed. May involve hypersensitivity to criticism, rejection, and blame; self-consciousness, comparisons, and insecurity around others; or a sense of shame regarding one's perceived flaws. These flaws may be private (e.g., selfishness, angry impulses, unacceptable sexual desires) or public (e.g., undesirable physical appearance, social awkwardness).",
         
-        "The feeling that one is isolated from the rest of the world, different from other people, and/or not part of any group or community.",
-        
+        "The belief that one has failed,  will inevitably fail, or is fundamentally inadequate relative to one's peers, in areas of achievement (school, career, sports, etc.). Often involves beliefs that one is stupid, inept, untalented, ignorant, lower in status, less successful than others, etc.",
+
         "Belief that one is unable to handle one's everyday responsibilities in a competent manner, without considerable help from others (e.g., take care of oneself, solve daily problems, exercise good judgment, tackle new tasks, make good decisions). Often presents as helplessness.",
         
         "Exaggerated fear that imminent catastrophe will strike at any time and that one will be unable to prevent it. Fears focus on one or more of the following:\n(A) Medical Catastrophes:  e.g., heart attacks, AIDS;\n(B) Emotional Catastrophes: e.g., going crazy; \nC): External Catastrophes: e.g., elevators collapsing, victimized by criminals, airplane crashes, earthquakes.",
         
         "Excessive emotional involvement and closeness with one or more significant others (often parents), at the expense of full individuation or normal social development.  Often involves the belief that at least one of the enmeshed individuals cannot survive or be happy without the constant support of the other.   May also include feelings of being smothered by, or fused with, others  OR  insufficient individual identity. Often experienced as a feeling of emptiness and floundering, having no direction, or in extreme cases questioning one's existence.",
         
-        "The belief that one has failed,  will inevitably fail, or is fundamentally inadequate relative to one's peers, in areas of achievement (school, career, sports, etc.). Often involves beliefs that one is stupid, inept, untalented, ignorant, lower in status, less successful than others, etc.",
-        
-        "The belief that one is superior to other people; entitled to special rights and privileges; or not bound by the rules of reciprocity that guide normal social interaction. Often involves insistence that one should be able to do or have whatever one wants, regardless of what is realistic, what others consider reasonable,  or the cost to others;  OR an exaggerated focus on superiority (e.g., being among  the most successful,  famous,  wealthy)  -- in order to achieve power or control (not primarily for attention or approval).  Sometimes includes excessive competitiveness toward, or domination of, others:  asserting one's power, forcing one's point of view, or controlling the behavior of others in line with one's own desires---without empathy or concern for others' needs or feelings.",
-        
-        "Pervasive difficulty or refusal to exercise sufficient self-control and frustration tolerance to achieve one's personal goals, or to restrain the excessive expression of one's emotions and impulses. In its milder form, patient presents with an exaggerated emphasis on discomfort-avoidance:  avoiding pain, conflict, confrontation, responsibility, or overexertion -- at the expense of personal fulfillment, commitment, or integrity.",
-        
         "Excessive surrendering of control to others because one feels coerced - - usually to avoid anger, retaliation, or abandonment. The two major forms of subjugation are: \nA. Subjugation of Needs:  Suppression of one's preferences, decisions, and desires. \nB. Subjugation of Emotions: Suppression of emotional expression, especially anger. \nUsually involves the perception that one's own desires, opinions,  and feelings are not valid or important to others. Frequently presents as excessive compliance, combined with hypersensitivity to feeling trapped. Generally leads to a build up of anger, manifested in maladaptive symptoms (e.g., passive-aggressive behavior, uncontrolled outbursts of temper, psychosomatic symptoms, withdrawal of affection, 'acting out', substance abuse).",
         
         "Excessive focus on voluntarily meeting the needs of others in daily situations, at the expense of one's own gratification.  The most common reasons are:  to prevent causing pain to others;  to avoid guilt from feeling selfish;  or to maintain the connection with others perceived as needy .  Often results from an acute sensitivity to the pain of others. Sometimes leads to a sense that one's own needs are not being adequately met and to resentment of those who are taken care of. (Overlaps with concept of codependency.)",
-        
-        "Excessive emphasis on gaining approval, recognition, or attention from other people, or fitting in, at the expense of developing a secure and true sense of self.  One's sense of esteem is dependent primarily on the reactions of others rather than on one's own natural inclinations.  Sometimes includes an overemphasis on status, appearance, social acceptance, money, or achievement --  as means of gaining approval, admiration, or attention (not primarily for power or control). Frequently results in major life decisions that are inauthentic or unsatisfying;  or in hypersensitivity to rejection.",
-        
-        "A pervasive, lifelong focus on the negative aspects of life (pain, death, loss, disappointment, conflict, guilt, resentment, unsolved problems, potential mistakes, betrayal, things that could go wrong, etc.) while minimizing or neglecting the positive or optimistic aspects. Usually includes an exaggerated expectation-- in a wide range of work, financial, or interpersonal situations -- that things will eventually go seriously wrong, or that aspects of one's life that seem to be going well will ultimately fall apart. Usually involves an inordinate fear of making mistakes that might lead to: financial collapse, loss, humiliation, or being trapped in a bad situation. Because potential negative outcomes are exaggerated, these patients are frequently characterized by chronic worry, vigilance, complaining, or indecision.",
         
         "The excessive inhibition of spontaneous action, feeling, or communication -- usually to avoid disapproval by others, feelings of shame, or losing control of one's impulses. The most common areas of inhibition involve:  (a) inhibition of anger & aggression;  (b) inhibition of positive impulses (e.g., joy, affection, sexual excitement, play);  (c) difficulty expressing vulnerability or communicating freely about one's feelings, needs, etc.;  or (d) excessive emphasis on rationality while disregarding emotions.",
         
         "The underlying belief that one must strive to meet very high internalized standards of behavior and performance, usually to avoid criticism. Typically results in feelings of pressure or difficulty slowing down; and in hypercriticalness toward oneself and others.  Must involve significant impairment in:  pleasure, relaxation, health, self-esteem, sense of accomplishment, or satisfying relationships. \nUnrelenting standards typically present as:  (a) perfectionism, inordinate attention to detail, or an underestimate of how good one's own performance is relative to the norm;  (b) rigid rules and “shoulds” in many areas of life, including unrealistically high moral, ethical, cultural, or religious precepts; or (c) preoccupation with time and efficiency, so that more can be accomplished.",
         
+        
+        "The belief that one is superior to other people; entitled to special rights and privileges; or not bound by the rules of reciprocity that guide normal social interaction. Often involves insistence that one should be able to do or have whatever one wants, regardless of what is realistic, what others consider reasonable,  or the cost to others;  OR an exaggerated focus on superiority (e.g., being among  the most successful,  famous,  wealthy)  -- in order to achieve power or control (not primarily for attention or approval).  Sometimes includes excessive competitiveness toward, or domination of, others:  asserting one's power, forcing one's point of view, or controlling the behavior of others in line with one's own desires---without empathy or concern for others' needs or feelings.",
+        
+        "Pervasive difficulty or refusal to exercise sufficient self-control and frustration tolerance to achieve one's personal goals, or to restrain the excessive expression of one's emotions and impulses. In its milder form, patient presents with an exaggerated emphasis on discomfort-avoidance:  avoiding pain, conflict, confrontation, responsibility, or overexertion -- at the expense of personal fulfillment, commitment, or integrity.",
+
+        
+        "Excessive emphasis on gaining approval, recognition, or attention from other people, or fitting in, at the expense of developing a secure and true sense of self.  One's sense of esteem is dependent primarily on the reactions of others rather than on one's own natural inclinations.  Sometimes includes an overemphasis on status, appearance, social acceptance, money, or achievement --  as means of gaining approval, admiration, or attention (not primarily for power or control). Frequently results in major life decisions that are inauthentic or unsatisfying;  or in hypersensitivity to rejection.",
+        
+        "A pervasive, lifelong focus on the negative aspects of life (pain, death, loss, disappointment, conflict, guilt, resentment, unsolved problems, potential mistakes, betrayal, things that could go wrong, etc.) while minimizing or neglecting the positive or optimistic aspects. Usually includes an exaggerated expectation-- in a wide range of work, financial, or interpersonal situations -- that things will eventually go seriously wrong, or that aspects of one's life that seem to be going well will ultimately fall apart. Usually involves an inordinate fear of making mistakes that might lead to: financial collapse, loss, humiliation, or being trapped in a bad situation. Because potential negative outcomes are exaggerated, these patients are frequently characterized by chronic worry, vigilance, complaining, or indecision.",
+
         "The belief that people should be harshly punished for making mistakes. Involves the tendency to be angry, intolerant,  punitive, and impatient with those people (including oneself) who do not meet one's expectations or standards.  Usually includes difficulty forgiving mistakes in oneself or others, because of a reluctance to consider extenuating circumstances, allow for human imperfection, or empathize with feelings."
         
     ]
